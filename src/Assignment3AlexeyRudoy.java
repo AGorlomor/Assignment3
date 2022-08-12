c4c7da43c11a0378e04bcd2619d6241179279820
AGorlomor <alexrudoy95@gmail.com>
2022-08-11 23:37:17 -0400

    Corrected the problem with the loop. The loop counter is being used as the subscript for the array, the stopping Modified to uses 16-sided dice and report total even and odd rolls Signed-Off: Alexey Rudoy rudo0007@algonquinlive.com

--- src/Assignment3AlexeyRudoy.java
+++ src/Assignment3AlexeyRudoy.java
@@ -1,6 +1,6 @@
 public class Assignment3AlexeyRudoy {
     public static void main(String[] args) {
-        int odd = 0;
+        int odds = 0;
         int roll = 0;
         int[] diceRolls = new int[10];
         int totalRolls = 0;
@@ -13,9 +13,9 @@ public class Assignment3AlexeyRudoy {
         for(int index = 0; index < diceRolls.length; index++) { // crashes
 
             if (diceRolls[index] %2 ==0){
-                odd++;
+                odds++;
             }
-            System.out.printf("Count of %d is: %d number of odd rolls : %d%n", (index + 1), diceRolls[index], odd);
+            System.out.printf("Count of %d is: %d number of odd rolls : %d%n", (index + 1), diceRolls[index], odds);
             totalRolls = totalRolls + diceRolls[index];
         }
         System.out.println("Total rolls were: " + totalRolls);
